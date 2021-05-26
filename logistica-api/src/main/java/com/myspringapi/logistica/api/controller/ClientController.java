896b58cb10056c5951451ecee08a74d500ea6b06
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-05-26 00:17:23 -0300

    Add client one and two in the list and return

--- logistica-api/src/main/java/com/myspringapi/logistica/api/controller/ClientController.java
+++ logistica-api/src/main/java/com/myspringapi/logistica/api/controller/ClientController.java
@@ -1,12 +1,29 @@
 package com.myspringapi.logistica.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myspringapi.logistica.domain.model.Client;
 
 @RestController
 public class ClientController {
 	@GetMapping("/clients")
-	public String show() {
-		return "Teste 1";
+	public List<Client> show() {
+		Client client1 = new Client();
+		client1.setId(1L);
+		client1.setName("Cliente one name");
+		client1.setEmail("clientoneemail@email.com");
+		client1.setPhone("55 44 333222111");
+		
+		Client client2 = new Client();
+		client2.setId(1L);
+		client2.setName("Cliente two name");
+		client2.setEmail("clientoneemail@email.com");
+		client2.setPhone("55 44 333222111");
+		
+		return Arrays.asList(client1, client2);
 	}
 }
