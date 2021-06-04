164b494ed4dc82e21dbd27c0af50b6ba7ab378f1
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-06-03 23:20:15 -0300

    Add notations of javapersistence to column names and turn client entity

--- logistica-api/src/main/java/com/myspringapi/logistica/domain/model/Client.java
+++ logistica-api/src/main/java/com/myspringapi/logistica/domain/model/Client.java
@@ -1,13 +1,32 @@
 package com.myspringapi.logistica.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded  = true)
 @Getter
 @Setter
+@Entity
 public class Client {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "email")
 	private String email;
+	
+	@Column(name = "phone_number")
 	private String phone;
 }
