f18ea25ebc88fb8ee82b556bcf4e5f70d5faada1
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-06-09 08:19:48 -0300

    using and add repository client interface

--- logistica-api/src/main/java/com/myspringapi/logistica/api/controller/ClientController.java
+++ logistica-api/src/main/java/com/myspringapi/logistica/api/controller/ClientController.java
@@ -2,23 +2,22 @@ package com.myspringapi.logistica.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myspringapi.logistica.domain.model.Client;
+import com.myspringapi.logistica.domain.repository.ClientRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClientController {
 	
-	@PersistenceContext
-	private EntityManager manager;
-	
+	private ClientRepository clientRepository;
+
 	@GetMapping("/clients")
 	public List<Client> show() {
-		return manager.createQuery("from Client", Client.class)
-				.getResultList();
+		return clientRepository.findAll();
 	}
 }
---
+++ logistica-api/src/main/java/com/myspringapi/logistica/domain/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.myspringapi.logistica.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.myspringapi.logistica.domain.model.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+}
